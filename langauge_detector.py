a2d40c52d96141e5b88a61fa53505c56feb997be
Akshay Yadav <akshay253101yadav@gmail.com>
2022-11-21 23:20:46 +0530

    Sort tags by commit date

--- langauge_detector.py
+++ langauge_detector.py
@@ -5,9 +5,10 @@ from language_graph import create_plot
 from language_analyzer import repo_languages
 
 # Save language analyses in csv
-def dump_language_data(path: str, headers: Set[str], langs: List[Dict[str,str]] ) -> str:
+def dump_language_data(path: str, headers: Set[str], langs: List[Dict[str, str]]) -> str:
     csv_path = path + ".csv"
-    if os.path.exists(csv_path): os.remove(csv_path)
+    if os.path.exists(csv_path):
+        os.remove(csv_path)
 
     with open(csv_path, 'w+', newline='') as file:
         writer = csv.DictWriter(file, headers)
@@ -16,10 +17,12 @@ def dump_language_data(path: str, headers: Set[str], langs: List[Dict[str,str]] 
             writer.writerow(lang)
         file.close()
 
+
 def analyze():
     (headers, langs, repo_name) = repo_languages()
     path = "./reports/" + repo_name + "-language"
     dump_language_data(path, headers, langs)
     create_plot(path)
+
 
 analyze()
